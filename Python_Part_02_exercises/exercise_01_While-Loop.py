5387224afd7fdeeb8f59ede73014d5f1d0d3a7be
Evi <evi.saloustrou>
2022-11-27 15:48:30 +0200

    While Loop. Exercise 01

---
+++ Python_Part_02_exercises/exercise_01_While-Loop.py
@@ -0,0 +1,18 @@
+# Iterators - Exercise 1 🐍
+
+"""
+create 
+`
+*
+**
+***
+****
+*****
+`
+this shape with print and while loop statemets
+"""
+
+i = 1
+while i <6:
+    print("*" * i)
+    i = i + 1
