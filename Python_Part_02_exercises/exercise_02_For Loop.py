b5dd6fd88e616aa3dca218f061b29440ec2caa43
Evi <evi.saloustrou>
2022-11-27 16:00:21 +0200

    For Loop. Exercise 02

---
+++ Python_Part_02_exercises/exercise_02_For Loop.py
@@ -0,0 +1,24 @@
+# Iterators - Exercise 2 🐍
+
+# create 
+# `
+# *
+# ***
+# *****
+# `
+# this shape with print, for loop, if and continue statements
+# """
+
+# for i in range(1,6):
+#     _ _%2==0:
+#         _
+#     _:
+#         _
+
+for i in range(1,6):
+    if i % 2 == 0:
+        continue
+    else:
+        print("*" * i)
+       
+
