5b180a95b4b93cc0dd585a4835954fb12c9f162e
Evi <evi.saloustrou>
2022-12-04 13:36:27 +0200

    First exersice on Python Functions

---
+++ Python_Part_02_exercises/Functions_Exercise_1.py
@@ -0,0 +1,18 @@
+# Functions - Exercise 1 🐍
+
+'''
+
+to create functions in python we start with `def` word and after it the `name of function` comes with without parameters  
+example is here 
+```
+def hello():
+    print("hello")
+```
+
+Now you create a function named goodbye without parameters which prints good bye   
+
+'''
+
+def hello():
+    print('good' + ' ' + 'bye')
+hello()
