3418ec2a8ceaa2a50ab4655ccc876fe6edf71a32
Evi <evi.saloustrou>
2022-12-27 20:50:51 +0200

    Exercise 1 - Classes and Object.py

--- Python_Part_02_exercises/Exercise 1 - Classes and Object.py
+++ Python_Part_02_exercises/Exercise 1 - Classes and Object.py
@@ -11,14 +11,16 @@ and then call `runs` method of it and it prints
 `"Running started"`
 
 '''
-class Animal:    
-    def __init__(self, runs, legs = 4):
-        self.runs = runs
-        self.legs = legs
+class Animal:
+    def __init__(self, leg_count):
+        self.leg_count = leg_count
+        print("Animal object was created")
+    
+    def runs(self):
+        print("Running started")
 
-animal = Animal(runs, 4)
-animal.runs(runs)
-print("Animal object was created")
+# create animal object with leg count
+animal = Animal(4)
 
-def runs(self):    
-    print("Running started")
+# call runs method of animal object
+animal.runs()
