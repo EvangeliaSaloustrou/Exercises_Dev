256cba06a6c0ab92f3d410bcc15e793629a6ee99
Evi <evi.saloustrou>
2022-12-27 20:31:50 +0200

    Methods_ex_3

--- Python_Part_01_exercises/Methods_ex_3.py
+++ Python_Part_01_exercises/Methods_ex_3.py
@@ -16,52 +16,41 @@
 
 num1 = 1122334455666
 
-# 1. cast num1 to string and assign to num1_str
-
+1) cast num1 to string and assign to num1_str
 num1_str = str(num1)
 
-# 2. check the length of the string
+2) check the length of the string
+print(len(num1_str))
 
-len(num1_str)
+3) get the third element of string (the one in the 3rd order)
+print(num1_str[2])
 
-# 3. get the third element of string (the one in the 3rd order)
+4) get the 3-5 elements of string (both inclusive) by string slicing
+print(num1_str[2:5])
 
-num1_str[3]
+5) check if num2 in string (cast if necessary)
+num2 = "223"
+if num2 in num1_str:
+    print("num2 found in num1_str")
+else:
+    print("num2 not found in num1_str")
 
-# 4. get the 3-5 elements of string (both inclusive) by string slicing
+6) check if num3 in string (cast if necessary)
+num3 = "999"
+if num3 in num1_str:
+    print("num3 found in num1_str")
+else:
+    print("num3 not found in num1_str")
 
-num1_str[3:5]
+7) concatenate 0 to the string from left and assign to string_with_0
+string_with_0 = "0" + num1_str
+print(string_with_0)
 
-# 5. check if num2 in string (cast if necessary)
-# num2 has not been given
-# let's assume that num2 = True
+8) get the characters of string_with_0 from start to position 4 (end point exclusive)
+print(string_with_0[:4])
 
-num2 = True
-print(type(num2))
-num2_str = str(num2)
-print(type(num2_str))
+9) get the characters of string_with_0 from position 5 until the end
+print(string_with_0[4:])
 
-# 6. check if num3 in string (cast if necessary)
-# num3 has not been given
-# let's assume that num3 = 33.2
-
-num3 = 33.2
-print(type(num3))
-num3_str = str(num3)
-print(type(num3_str))
-
-# 7. concatenate 0 to the string from left and assign to string_with_0
-
-string_with_0 = "0" + num1_str 
-
-# 8. get the characters of string_with_0 from start to position 4 (end point exclusive)
-
-string_with_0[:5]
-
-# 9. get the characters of string_with_0 from position 5 until the end
-
-string_with_0[5:]
-
-# 10. use negative indexing to reach the "567" string_with_0
-
-string_with_0[-9:-6]
+10) use negative indexing to reach the "567" string_with_0
+print(string_with_0[-3:])
